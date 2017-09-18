e575109466c3d7c1dc8b7a8feb5b2c8d7bd1a4e9
tetris <tang.raphael@gmail.com>
2017-09-17 21:55:01 -0400

    Improve model (#7)
    * Add Watson TTS option
    
    - pyttsx is buggy on some distros of Linux
    
    * Add more confusing training data
    
    * Train improved model
    
    * Change labelling algorithm

--- utils/client.py
+++ utils/client.py
@@ -4,6 +4,7 @@ from tkinter import *
 from tkinter.ttk import *
 import argparse
 import base64
+import io
 import json
 import os
 import PIL.ImageTk as itk
@@ -13,6 +14,7 @@ import re
 import requests
 import speech_recognition as sr
 import time
+import wave
 import zlib
 
 class GooseWindow(Thread):
@@ -55,31 +57,77 @@ class GooseWindow(Thread):
         self._switch = not self._switch
 
 def clean_text(text):
+    if not text:
+        return ""
     pattern = re.compile(r"^([A-z0-9,\.]+(\-*[A-z0-9,\.]+)*)+$")
     words = []
     for tok in text.split():
         if re.match(pattern, tok):
             words.append(tok)
-    return " ".join(words)
+    return " ".join(words).replace(" .", ".")
+
+class WatsonApi(object):
+    def __init__(self, username, password):
+        self.auth = requests.auth.HTTPBasicAuth(username, password)
+
+    def fetch_tts(self, text):
+        ep = "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize"
+        response = requests.get(ep, params=dict(accept="audio/wav", text=text, voice="en-US_AllisonVoice"), stream=True, auth=self.auth)
+        buf = io.BytesIO()
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                buf.write(chunk)
+        buf.seek(0)
+        return buf
+
+def play_audio(data, amplitude_cb=None):
+    with wave.open(data) as f:
+        audio = pyaudio.PyAudio()
+        stream = audio.open(
+            format=audio.get_format_from_width(f.getsampwidth()),
+            channels=f.getnchannels(),
+            rate=f.getframerate(),
+            output=True)
+        data = f.readframes(512)
+        while data:
+            if amplitude_cb:
+                amplitude = AudioSnippet(data).amplitude_rms()
+                amplitude_cb(amplitude)
+            stream.write(data)
+            data = f.readframes(512)
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
 
 class Client(object):
-    def __init__(self, listen_endpoint, qa_endpoint, goose_window):
+    def __init__(self, listen_endpoint, qa_endpoint, goose_window, watson_api=None):
+        self.watson_api = watson_api
         self.listen_endpoint = listen_endpoint
         self.qa_endpoint = qa_endpoint
         self.chunk_size = 16000
         self.recognizer = sr.Recognizer()
         self.goose_window = goose_window
-        self._tts = pyttsx3.init()
-        self._tts.connect("started-word", self._make_tts_cb())
+        if not watson_api:
+            self._tts = pyttsx3.init()
+            self._tts.connect("started-word", self._make_tts_cb())
 
     def say_text(self, text):
-        self._tts.say(text)
-        self._tts.runAndWait()
+        if self.watson_api:
+            data = self.watson_api.fetch_tts(text)
+            play_audio(data, amplitude_cb=self._make_tts_cb())
+        else:
+            self._tts.say(text)
+            self._tts.runAndWait()
 
     def _make_tts_cb(self):
         def on_start(name, location, length):
             self.goose_window.open_mouth(length / 40)
-        return on_start
+        def on_amplitude(amplitude):
+            if amplitude > 0.05:
+                self.goose_window.draw_goose("open")
+            else:
+                self.goose_window.draw_goose("awake")
+        return on_amplitude if self.watson_api else on_start
 
     def contains_command(self, data):
         data = base64.b64encode(zlib.compress(data))
@@ -126,8 +174,11 @@ class Client(object):
                 question = self._recognize_speech()
                 print("You asked, \"{}\"".format(question))
                 answer = clean_text(self.query_qa(question))
-                print("Answer: {}".format(answer))
-                self.say_text(answer)
+                if answer:
+                    print("Answer: {}".format(answer))
+                    self.say_text(answer)
+                else:
+                    print("No answer available!")
 
                 buf = [self.stream.read(self.chunk_size), self.stream.read(self.chunk_size)]
                 self.goose_window.draw_goose("inactive")
@@ -146,10 +197,23 @@ def main():
         "--qa-endpoint",
         type=str,
         default="http://dragon00.cs.uwaterloo.ca:80")
+    parser.add_argument(
+        "--watson-username",
+        type=str,
+        default="",
+        help="If supplied, uses Watson's TTS")
+    parser.add_argument(
+        "--watson-password",
+        type=str,
+        default="",
+        help="If supplied, uses Watson's TTS")
     flags, _ = parser.parse_known_args()
     file_dir = os.path.dirname(os.path.realpath(__file__))
     goose_window = GooseWindow(file_dir)
-    client = Client(flags.listen_endpoint, flags.qa_endpoint, goose_window)
+    watson_api = None
+    if flags.watson_username and flags.watson_password:
+        watson_api = WatsonApi(flags.watson_username, flags.watson_password)
+    client = Client(flags.listen_endpoint, flags.qa_endpoint, goose_window, watson_api)
     client.start_live_qa()
 
 if __name__ == "__main__":
