0ebe149323c877c549bbd4146c0aca9fc9af78d3
daemon <0x8badc0de@gmail.com>
2017-10-01 16:47:10 -0400

    Fix bugs

--- utils/model.py
+++ utils/model.py
@@ -3,7 +3,9 @@ import argparse
 import os
 import random
 
+from torch.autograd import Variable
 import librosa
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -15,7 +17,7 @@ class ConfigBuilder(object):
 
     def build_argparse(self):
         parser = argparse.ArgumentParser()
-        for key, value in conf.items():
+        for key, value in self.default_config.items():
             key = "--{}".format(key)
             if isinstance(value, tuple):
                 parser.add_argument(key, default=list(value), nargs=len(value), type=type(value[0]))
@@ -42,8 +44,8 @@ class SpeechModel(nn.Module):
         conv2_size = config["conv2_size"]
         conv1_pool = config["conv1_pool"]
         conv2_pool = config["conv2_pool"]
-        conv1_stride = config["conv1_stride"]
-        conv2_stride = config["conv2_stride"]
+        conv1_stride = tuple(config["conv1_stride"])
+        conv2_stride = tuple(config["conv2_stride"])
         linear_size = config["linear_size"]
         dnn_size = config["dnn_size"]
         width = config["width"]
@@ -52,10 +54,18 @@ class SpeechModel(nn.Module):
         self.pool1 = nn.MaxPool2d(conv1_pool)
         self.conv2 = nn.Conv2d(n_featmaps1, n_featmaps2, conv2_size, stride=conv2_stride)
         self.pool2 = nn.MaxPool2d(conv2_pool)
+
+        m1, m2 = conv1_size
+        s, v = conv1_stride
+        p, q = conv1_pool
+        h = ((height - m1 + 1) // (s * p)) 
+        w = ((width - m2 + 1) // (v * q))
+
         m1, m2 = conv2_size
         s, v = conv2_stride
         p, q = conv2_pool
-        conv_net_size = ((width - m1 + 1) // (s * p)) * ((height - m2 + 1) // (v * q))
+        conv_net_size = ((h - m1 + 1) // (s * p)) * ((w - m2 + 1) // (v * q))
+
         self.linear = nn.Linear(n_featmaps2 * conv_net_size, linear_size)
         self.dnn = nn.Linear(linear_size, dnn_size)
         self.output = nn.Linear(dnn_size, n_labels)
@@ -63,8 +73,10 @@ class SpeechModel(nn.Module):
     @staticmethod
     def default_config():
         # Full arch (~9.7M params)
-        # TODO: initialize width and height
         config = {}
+        config["height"] = 101
+        config["width"] = 40
+        config["n_labels"] = 5
         config["n_feature_maps1"] = 64
         config["n_feature_maps2"] = 64
         config["conv1_size"] = (20, 8)
@@ -93,11 +105,12 @@ class SpeechDataset(data.Dataset):
         super().__init__()
         self.audio_files = list(data.keys())
         self.audio_labels = list(data.values())
-        self.bg_noise_audio = [librosa.core.load(file, sr=16000) for file in config.get("bg_noise_files", [])]
+        config["bg_noise_files"] = list(filter(lambda x: x.endswith("wav"), config.get("bg_noise_files", [])))
+        self.bg_noise_audio = [librosa.core.load(file, sr=16000)[0] for file in config["bg_noise_files"]]
         self.unknown_prob = config["unknown_prob"]
         self.silence_prob = config["silence_prob"]
         self.noise_prob = config["noise_prob"]
-        self.n_dct = config["n_dict_filters"]
+        self.n_dct = config["n_dct_filters"]
         self.filters = librosa.filters.dct(config["n_dct_filters"], config["n_mels"])
         self.n_mels = config["n_mels"]
 
@@ -106,21 +119,21 @@ class SpeechDataset(data.Dataset):
         config = {}
         config["silence_prob"] = 0.1
         config["noise_prob"] = 0.8
-        config["n_dct_filters"] = 13
+        config["n_dct_filters"] = 40
         config["input_length"] = 16000
         config["n_mels"] = 40
-        config["unknown_prob"] = 0.1
+        config["unknown_prob"] = 0.01
         config["train_pct"] = 80
         config["dev_pct"] = 10
         config["test_pct"] = 10
-        config["wanted_words"] = ["command", "random"]
-        config["data_folder"] = "/tmp/speech_dataset"
+        config["wanted_words"] = ["left", "bed", "random"]
+        config["data_folder"] = "/data/speech_dataset"
         return config
 
-    def preprocess(self, example, bg_noise):
-        bg_noise = random.choice(self.bg_noise_audio)
-        a = random.randint(0, len(bg_noise) - 16000 - 1)
-        if bg_noise:
+    def preprocess(self, example):
+        if self.bg_noise_audio:
+            bg_noise = random.choice(self.bg_noise_audio)
+            a = random.randint(0, len(bg_noise) - 16000 - 1)
             bg_noise = bg_noise[a:a + 16000]
         else:
             bg_noise = np.zeros(16000)
@@ -130,13 +143,14 @@ class SpeechDataset(data.Dataset):
             is_silence = True
             data = bg_noise
         else:
-            data = librosa.core.load(example, sr=16000)
+            data = librosa.core.load(example, sr=16000)[0]
+            data = np.pad(data, (0, max(0, 16000 - len(data))), "constant")
             if random.random() < self.noise_prob:
                 a = random.random() * 0.3
                 data = a * bg_noise + (1 - a) * data
-        data = np.log(librosa.feature.melspectrogram(data[0], sr=data[1], n_mels=self.n_mels, hop_length=160, n_fft=400))
-        data = data.transpose()
-        data = np.array([np.matmul(self.filters, x)[1:1 + self.n_dct] for x in np.split(data)])
+        data = np.log(librosa.feature.melspectrogram(data, sr=16000, n_mels=self.n_mels, hop_length=160, n_fft=400))
+        data = [np.matmul(self.filters, x) for x in np.split(data, data.shape[1], axis=1)]
+        data = np.array(data, order="F").squeeze(2).astype(np.float32)
         data = torch.from_numpy(data) # shape: (frames, dct_coeffs)
         return data, is_silence
 
@@ -150,10 +164,10 @@ class SpeechDataset(data.Dataset):
         test_pct = config["test_pct"]
 
         words = {word: i + 2 for i, word in enumerate(wanted_words)}
-        words.update(dict(cls.LABEL_SILENCE=0, cls.LABEL_UNKNOWN=1))
+        words.update({cls.LABEL_SILENCE:0, cls.LABEL_UNKNOWN:1})
         files = {0: {}, 1: {}, 2: {}}
         bg_noise_files = []
-
+        
         for folder_name in os.listdir(folder):
             path_name = os.path.join(folder, folder_name)
             is_bg_noise = False
@@ -180,7 +194,7 @@ class SpeechDataset(data.Dataset):
                     tag = 1
                 else:
                     tag = 2
-                files[tag][wav_name] = words[label]
+                files[tag][wav_name] = label
         train_cfg = ChainMap(dict(bg_noise_files=bg_noise_files), config)
         test_cfg = ChainMap(dict(noise_prob=0), config)
         return (cls(files[0], train_cfg), cls(files[1], test_cfg), cls(files[2], test_cfg))
@@ -196,7 +210,8 @@ class SpeechDataset(data.Dataset):
 def print_eval(name, scores, labels, loss, end="\n"):
     batch_size = labels.size(0)
     accuracy = (torch.max(scores, 1)[1].view(batch_size).data == labels.data).sum() / batch_size
-    print("{} accuracy: {:>15}, loss: {:>15}".format(name, accuracy, loss), end=end)
+    loss = loss.cpu().data.numpy()[0]
+    print("{} accuracy: {:>5}, loss: {:>15}".format(name, accuracy, loss), end=end)
 
 def train(config):
     train_set, dev_set, test_set = SpeechDataset.splits(config)
@@ -204,38 +219,54 @@ def train(config):
     if not config["no_cuda"]:
         torch.cuda.set_device(config["gpu_no"])
         model.cuda()
-    optimizer = nn.optim.Adadelta(model.parameters(), lr=config["lr"])
-    criterion = nn.optim.CrossEntropyLoss()
+    optimizer = torch.optim.Adadelta(model.parameters(), lr=config["lr"])
+    criterion = nn.CrossEntropyLoss()
     
     train_loader = data.DataLoader(train_set, batch_size=config["batch_size"], shuffle=True, drop_last=True)
     dev_loader = data.DataLoader(dev_set, batch_size=len(dev_set))
     test_loader = data.DataLoader(test_set, batch_size=len(test_set))
 
-    for epoch_idx in config["n_epochs"]:
-        for batch_idx, (model_in, labels) in train_loader:
+    for epoch_idx in range(config["n_epochs"]):
+        for batch_idx, (model_in, labels) in enumerate(train_loader):
             model.train()
             optimizer.zero_grad()
             if not config["no_cuda"]:
                 model_in = model_in.cuda()
+                labels = labels.cuda()
+            model_in = Variable(model_in, requires_grad=False)
             scores = model(model_in)
+            labels = Variable(labels, requires_grad=False)
             loss = criterion(scores, labels)
             loss.backward()
-            optimizer.step()
+            opitimizer.step()
             print_eval("train", scores, labels, loss, end="\r")
+        print()
+
         if epoch_idx % 100 == 99:
             model.eval()
             for model_in, labels in dev_loader:
+                model_in = Variable(model_in, requires_grad=False)
+                if not config["no_cuda"]:
+                    model_in = model_in.cuda()
+                    labels = labels.cuda()
                 scores = model(model_in)
+                labels = Variable(labels, requires_grad=False)
                 loss = criterion(scores, labels)
                 print_eval("dev", scores, labels, loss)
+
     for model_in, labels in test_loader:
+        model_in = Variable(model_in, requires_grad=False)
+        if not config["no_cuda"]:
+            model_in = model_in.cuda()
+            labels = labels.cuda()
         scores = model(model_in)
+        labels = Variable(labels, requires_grad=False)
         loss = criterion(scores, labels)
         print_eval("test", scores, labels, loss)
 
 def main():
-    global_config = dict(no_cuda=False, n_epochs=1200, lr=0.001, mode="train", batch_size=100,
-        input_file="", output_file="", gpu_no=0)
+    global_config = dict(no_cuda=False, n_epochs=1200, lr=0.01, mode="train", batch_size=100,
+        input_file="", output_file="", gpu_no=1)
     config = ConfigBuilder(
         SpeechModel.default_config(),
         SpeechDataset.default_config(),
