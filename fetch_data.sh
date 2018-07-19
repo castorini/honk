e36acebfea699114eb1044ba16d0a024eccb2f5a
Chris Lovett <clovett@microsoft.com>
2018-07-18 19:33:40 -0700

    Add alternative non-GLUT demo (#53)
    - Add a Tkinter-based demo for folks who want to run a demo without installing GLUT.

--- fetch_data.sh
+++ fetch_data.sh
@@ -4,10 +4,10 @@ wget -nc https://github.com/castorini/honk-data/archive/master.zip -O data.zip
 wget -nc https://github.com/castorini/honk-models/archive/master.zip -O models.zip
 
 echo "Extracting honk data..."
-yes no | unzip data.zip > /dev/null
-yes no | unzip models.zip > /dev/null
-mv honk-data-master/training_data . 2> /dev/null
-mv -T honk-models-master model 2> /dev/null
+yes no | unzip data.zip
+yes no | unzip models.zip
+mv honk-data-master/training_data .
+mv -T honk-models-master model
 
 echo "Cleaning up..."
 rm -rf honk-models-master
