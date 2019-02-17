55464167a5c5ab9ead0324d9860f4d22c31c6929
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-02-17 16:22:11 -0500

    removing unnecessary files

--- keyword_spotting_data_generator/evaluation/similar_audio_retractor/similarity_metric.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-class SimilarityMetric():
-    def __init__(self, target):
-        self.target = np.array(target)
-
-    def compute_simularity(self, data, target=None):
-        pass
